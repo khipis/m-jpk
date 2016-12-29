716635070890d91e08de180d182a8f19466ba8e9
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2016-12-29 17:27:34 +0100

    Add possibility to pass config file through cli args

--- src/main/java/pl/softcredit/mpjk/client/JpkCLI.java
+++ src/main/java/pl/softcredit/mpjk/client/JpkCLI.java
@@ -13,8 +13,7 @@ import static pl.softcredit.mpjk.engine.JpkProcessors.FORMAL_VALIDATION_PROCESSO
 public class JpkCLI {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JpkCLI.class);
-    private static final ConfigurationService configurationService =
-            new ConfigurationService("G:\\work\\m-jpk\\src\\main\\resources\\config.properties");
+    private static ConfigurationService configurationService;
 
     private JpkCLI(JpkProcessor... processors) throws JpkException {
         for (JpkProcessor processor : processors) {
@@ -23,7 +22,15 @@ public class JpkCLI {
     }
 
     public static void main(String[] args) {
-       try {
+
+        String configFilePath = "G:\\work\\m-jpk\\src\\main\\resources\\config.properties";
+        if (args != null && args.length >= 1) {
+            configFilePath = args[0];
+        }
+
+        configurationService = new ConfigurationService(configFilePath);
+
+        try {
             new JpkCLI(FORMAL_VALIDATION_PROCESSOR);
         } catch (JpkException e) {
             LOGGER.error("Cannot perform validation: " + e.getMessage());
