df547efb65e14ac141916b62f0bce06ae1b8b180
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2017-01-01 12:27:13 +0100

    Final formal validation -Fix log4j config,

--- src/main/java/pl/softcredit/mpjk/client/JpkCLI.java
+++ src/main/java/pl/softcredit/mpjk/client/JpkCLI.java
@@ -3,27 +3,23 @@ package pl.softcredit.mpjk.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import pl.softcredit.mpjk.core.configuration.ConfigurationLoader;
 import pl.softcredit.mpjk.core.configuration.JpkConfiguration;
 import pl.softcredit.mpjk.engine.JpkExecutor;
 
+import static pl.softcredit.mpjk.core.configuration.ConfigurationLoader.load;
 import static pl.softcredit.mpjk.engine.processors.JpkProcessors.CLEAN_WORKING_DIRECTORY_PROCESSOR;
 import static pl.softcredit.mpjk.engine.processors.JpkProcessors.FORMAL_VALIDATION_PROCESSOR;
 
 public class JpkCLI {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JpkCLI.class);
-    private static JpkConfiguration config;
 
     public static void main(String[] args) {
-
-        config = ConfigurationLoader.load(args);
+        JpkConfiguration config = load(args);
 
         new JpkExecutor(config).execute(
                 CLEAN_WORKING_DIRECTORY_PROCESSOR,
                 FORMAL_VALIDATION_PROCESSOR
         );
-
     }
-
 }
