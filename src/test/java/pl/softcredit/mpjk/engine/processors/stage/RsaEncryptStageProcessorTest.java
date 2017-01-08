5c49a570028e874f7a9111dd1641f74ba5b4a1a3
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2017-01-08 12:48:03 +0100

    Add rsa encrypt stage processor

---
+++ src/test/java/pl/softcredit/mpjk/engine/processors/stage/RsaEncryptStageProcessorTest.java
@@ -0,0 +1,55 @@
+package pl.softcredit.mpjk.engine.processors.stage;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import pl.softcredit.mpjk.core.configuration.JpkConfiguration;
+import pl.softcredit.mpjk.engine.processors.JpkProcessor;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.commons.io.FileUtils.cleanDirectory;
+import static org.mockito.Mockito.when;
+import static pl.softcredit.mpjk.engine.TestPaths.JPK_VAT_SCHEME_FILE;
+import static pl.softcredit.mpjk.engine.TestPaths.RESOURCES_INPUT_FILES;
+import static pl.softcredit.mpjk.engine.TestPaths.RSA_KEY_FILE_NAME;
+import static pl.softcredit.mpjk.engine.TestPaths.SCHEMES_DIR;
+import static pl.softcredit.mpjk.engine.TestPaths.TEMP_WORKING_DIR;
+import static pl.softcredit.mpjk.engine.TestPaths.VALID_FILE_NAME;
+import static pl.softcredit.mpjk.engine.TestPaths.assertFile;
+import static pl.softcredit.mpjk.engine.processors.JpkProcessors.RSA_ENCRYPT_STAGE_PROCESSOR;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RsaEncryptStageProcessorTest {
+
+    @Mock
+    private JpkConfiguration config;
+
+    private JpkProcessor rsaGeneratorStageProcessor = RSA_ENCRYPT_STAGE_PROCESSOR;
+
+    @Before
+    public void setUp() throws IOException {
+        cleanDirectory(new File(TEMP_WORKING_DIR));
+        whenConfigurationWith(VALID_FILE_NAME, JPK_VAT_SCHEME_FILE);
+    }
+
+    @Test
+    public void shouldSaveFileWithGeneratedRsaKeyIntoWorkingDirectory() throws Exception {
+        rsaGeneratorStageProcessor.process(config);
+
+       assertFile(RSA_KEY_FILE_NAME);
+    }
+
+    private void whenConfigurationWith(String inputFile, String schemeFile) {
+        when(config.getWorkingDirectoryPath()).thenReturn(TEMP_WORKING_DIR);
+        when(config.getSchemeFilePath()).thenReturn(SCHEMES_DIR + schemeFile);
+        when(config.getInputFilePath()).thenReturn(RESOURCES_INPUT_FILES + inputFile);
+    }
+
+
+
+}
