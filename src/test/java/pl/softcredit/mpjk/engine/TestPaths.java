f4a6b72e5d66178f114085bc899cf22f05ad48f0
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2017-01-08 10:49:52 +0100

    Add new tests paths

--- src/test/java/pl/softcredit/mpjk/engine/TestPaths.java
+++ src/test/java/pl/softcredit/mpjk/engine/TestPaths.java
@@ -14,6 +14,7 @@ public class TestPaths {
     public static final String ZIP_FILE_NAME = "valid_JPK_VAT(1)_file.xml.zip";
     public static final String AES_FILE_NAME = "valid_JPK_VAT(1)_file.xml.zip.aes";
     public static final String KEY_FILE_NAME = "valid_JPK_VAT(1)_file.key";
+    public static final String SHA_FILE_NAME = "valid_JPK_VAT(1)_file.sha";
     public static final String VEC_FILE_NAME = "valid_JPK_VAT(1)_file.vec";
     public static final String INVALID_FILE = "invalid_JPK_VAT(1)_file.xml";
     public static final String JPK_VAT_SCHEME_FILE = "Schemat_JPK_VAT(1)_v1-0.xsd";
@@ -21,6 +22,8 @@ public class TestPaths {
     public static final String TEMP_FILE_NAME = "tempfile.xml";
     public static final String TEMP_INPUT_FILE = TEMP_WORKING_DIR + separator + "tempfile.xml";
     public static final String VALID_FILE_PATH_FROM_RESOURCES = RESOURCES_INPUT_FILES + separator + VALID_FILE_NAME;
+    public static final String AES_FILE_PATH_FROM_RESOURCES = RESOURCES_INPUT_FILES + separator + AES_FILE_NAME;
+    public static final String AES_FILE_PATH_FROM_MF = RESOURCES_INPUT_FILES + separator + "JPK-VAT-TEST-0000.xml.zip.aes";
     public static final String RSA_KEY_FILE_PATH_FROM_RESOURCES = "src/test/resources/resources/JPKMFTest-klucz publiczny do szyfrowania.pem";
     public static final String AES_FILE_PATH = TEMP_WORKING_DIR + separator + AES_FILE_NAME;
     public static final String ZIPPED_FILE_PATH = TEMP_WORKING_DIR + separator + VALID_FILE_NAME + ZIP_EXTENSION;
